6491be69da42c5e5ce9bc7dda0ad9b2855e8efac
Ekzz0 <92272824+Ekzz0@users.noreply.github.com>
2022-05-03 16:54:16 +0300

    first version
    The first version of the program has been created so that everyone can continue working using it

---
+++ Clicker Drill/Assets/Scripts/AchScene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AchScene : MonoBehaviour
+{
+    public int totalMoney; // общее число очков
+    [SerializeField] Button firstAchive;
+    [SerializeField] bool isFitstAchive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+        totalMoney = PlayerPrefs.GetInt("totalMoney"); 
+        isFitstAchive = PlayerPrefs.GetInt("isFitstAchive") == 1 ? true : false;
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (totalMoney >= 10 && !isFitstAchive)
+        {
+            firstAchive.interactable = true; // Если условие выполнено, то на кнопку можно нажать
+        }
+        else
+        {
+            firstAchive.interactable = false;
+        }
+    }
+
+    public void GetFirstAchive()
+    {
+        int total_Money = PlayerPrefs.GetInt("totalMoney");
+        total_Money += 10;
+        PlayerPrefs.SetInt("totalMoney", total_Money);
+        isFitstAchive = true;
+        PlayerPrefs.SetInt("isFitstAchive", isFitstAchive ? 1 : 0);
+    }
+    public void moveToMainScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Clicker Drill/Assets/Scripts/MainScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MainScene : MonoBehaviour
+{
+    [SerializeField] int money;
+    public Text moneyText;
+    public int totalMoney;
+    public void Buttonclick()
+    {
+        totalMoney++;
+        PlayerPrefs.SetInt("totalMoney", totalMoney); // сохранение денег в памяти
+    }
+
+    
+    public void moveToAchivements() // Переход на сцену достижений
+    {
+        SceneManager.LoadScene(1);
+    }
+    private void Start()
+    {
+        //PlayerPrefs.SetInt("isFitstAchive", 0); //Чтобы обнулить 1ю ачивку
+        //PlayerPrefs.SetInt("totalMoney", 0);  // Это для сброса кол-ва денег
+        totalMoney = PlayerPrefs.GetInt("totalMoney");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        moneyText.text = totalMoney.ToString();
+        
+    }
+}
